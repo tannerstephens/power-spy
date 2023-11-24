f6691931811a43a243389a3efdc7ab4c0801ac31
Tanner Stephens <tannerrstephens@gmail.com>
2023-11-24 02:40:37 -0600

    Login help text

--- power_spy/static/js/login.js
+++ power_spy/static/js/login.js
@@ -1,9 +1,12 @@
 window.onload = () => {
     const passwordField = document.getElementById('password');
     const form = document.getElementById('form');
+    const invalid = document.getElementById('invalid');
 
     form.onsubmit = e => {
         e.preventDefault();
+
+        invalid.classList.add('is-invisible')
 
         fetch('/api/login', {
             method: 'POST',
@@ -16,6 +19,8 @@ window.onload = () => {
             .then(data => {
                 if(data.success) {
                     window.location = '/'
+                } else {
+                    invalid.classList.remove('is-invisible')
                 }
             })
     }
